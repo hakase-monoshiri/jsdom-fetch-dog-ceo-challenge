60f54cd49fecc921e7549529d4bcb12d3dbfd741
hakase-monoshiri <ebenaeleazer@outlook.com>
2021-12-08 12:59:16 -0500

    commit tree 91a450711a99cf8dfa6e6448b1920d6a64301b1f

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 console.log('%c HI', 'color: firebrick')
 
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+fetch(imgUrl).then( 
+    resp => resp.json() )
+    .then(
+        obj => {
+            obj.map( img => {
+                const dogContainer = document.getElementById("dog-image-container");
+                dogContainer.appendChild(img);
+            })
+        }
+    )
